94610f5620ff16e79f038638fb942a5d22106a9f
Shuta-syd <shuta.ogura0921@gmail.com>
2022-09-13 16:58:33 +0900

    [mod] player put -> my_mlx_pixel_put

--- inc/cub3D.h
+++ inc/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 22:34:35 by shogura           #+#    #+#             */
-/*   Updated: 2022/09/13 15:14:32 by shogura          ###   ########.fr       */
+/*   Updated: 2022/09/13 16:26:02 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,12 @@
 // @window size 4:3 128:96
 # define WIN_W 1024
 # define WIN_H 768
+
+// @ keycode
+# define KEY_W 119
+# define KEY_A 97
+# define KEY_S 115
+# define KEY_D 100
 
 // @message
 # define ERROR "[ERROR] something wrong"
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 20:25:04 by shogura           #+#    #+#             */
-/*   Updated: 2022/09/13 16:23:11 by shogura          ###   ########.fr       */
+/*   Updated: 2022/09/13 16:57:51 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,16 +46,27 @@ void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-int	keyAction(t_data *dt){
-	(void)dt;
+int	keyAction(int key, t_data *dt){
+	if (key == KEY_W)
+		dt->Tvec.y -= 5;
+	else if (key == KEY_S)
+		dt->Tvec.y += 5;
+	else if (key == KEY_A)
+		dt->Tvec.x -= 5;
+	else if (key == KEY_D)
+		dt->Tvec.x += 5;
 	return (0);
 }
 
 void drawPlayer(t_data *dt, int x, int y)
 {
-	for (size_t i = 0; i < 10; i++)
-		for (size_t j = 0; j < 10; j++)
-			mlx_pixel_put(dt->Tmlx.mlx, dt->Tmlx.win, x + j, y + i, 0xFFFF00);
+	t_img img = dt->Timgs.img3;
+	img.img = mlx_new_image(dt->Tmlx.mlx, 15, 15);
+	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_len, &img.endian);
+	for (size_t i = 0; i < 15; i++)
+		for (size_t j = 0; j < 15; j++)
+			my_mlx_pixel_put(&img, x + j, y + i, 0x00FFFF00);
+	mlx_put_image_to_window(dt->Tmlx.mlx, dt->Tmlx.win, img.img, x, y);
 }
 
 int drawMap2D(t_data *dt)
@@ -65,6 +76,7 @@ int drawMap2D(t_data *dt)
 	{
 		for (size_t j = 0; j < 8; j++)
 		{
+			drawPlayer(dt, dt->Tvec.x, dt->Tvec.y);
 			int x = j * 128;
 			int y = i * 96;
 			if (basicMap[i][j] == '1')
@@ -73,7 +85,6 @@ int drawMap2D(t_data *dt)
 				mlx_put_image_to_window(dt->Tmlx.mlx, dt->Tmlx.win, dt->Timgs.img2.img, x, y);
 		}
 	}
-	drawPlayer(dt, dt->Tvec.x, dt->Tvec.y);
 	return (0);
 }
 
@@ -87,7 +98,6 @@ int	main(int argc, char const *argv[])
 	if (dt == NULL)
 		ft_error(NULL);
 	init(dt);
-	drawMap2D(dt);
 	mlx_loop_hook(dt->Tmlx.mlx, drawMap2D, dt);
 	mlx_hook(dt->Tmlx.win, 2, 1L << 0, keyAction, dt);
 	mlx_hook(dt->Tmlx.win, 17, 1L << 2, destroyWindow, dt);

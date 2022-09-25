d855fd4b38d92a201a09031e6eba99e1cb1e2cc4
Shuta-syd <shuta.ogura0921@gmail.com>
2022-09-25 21:20:22 +0900

    [ref] init.c

--- src/init.c
+++ src/init.c
@@ -6,39 +6,40 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 17:49:48 by shogura           #+#    #+#             */
-/*   Updated: 2022/09/25 20:00:55 by shogura          ###   ########.fr       */
+/*   Updated: 2022/09/25 21:20:15 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 
-void	initImage(t_data *dt, t_imgs *Timg, t_mlx *M)
+void	init_image(t_data *dt, t_imgs *Timg, t_mlx *M)
 {
 	t_img	*map;
-	t_img	*map3D;
-	t_img	*P;
+	t_img	*map3d;
+	t_img	*p;
 
 	map = &Timg->map;
-	P = &Timg->P;
-	map3D = &Timg->map3D;
-
-	map->img = mlx_new_image(M->mlx, WINDOW_W * MINIMAP_SCALE, WINDOW_H * MINIMAP_SCALE);
-	map->addr = mlx_get_data_addr(map->img, &map->bpp, &map->line_len, &map->endian);
-
-	P->img = mlx_new_image(M->mlx, dt->P.width * MINIMAP_SCALE, dt->P.height * MINIMAP_SCALE);
-	P->addr = mlx_get_data_addr(P->img, &P->bpp, &P->line_len, &P->endian);
-
-	map3D->img = mlx_new_image(M->mlx, WINDOW_W, WINDOW_H);
-	map3D->addr = mlx_get_data_addr(map3D->img, &map3D->bpp, &map3D->line_len, &map3D->endian);
+	p = &Timg->P;
+	map3d = &Timg->map3D;
+	map->img = mlx_new_image(M->mlx, WINDOW_W * MINIMAP_SCALE,
+			WINDOW_H * MINIMAP_SCALE);
+	map->addr = mlx_get_data_addr(map->img, &map->bpp,
+			&map->line_len, &map->endian);
+	p->img = mlx_new_image(M->mlx, dt->P.width * MINIMAP_SCALE,
+			dt->P.height * MINIMAP_SCALE);
+	p->addr = mlx_get_data_addr(p->img, &p->bpp, &p->line_len, &p->endian);
+	map3d->img = mlx_new_image(M->mlx, WINDOW_W, WINDOW_H);
+	map3d->addr = mlx_get_data_addr(map3d->img, &map3d->bpp,
+		&map3d->line_len, &map3d->endian);
 }
 
-void	initMlx(t_mlx *M)
+void	init_mlx(t_mlx *M)
 {
 	M->mlx = mlx_init();
 	M->win = mlx_new_window(M->mlx, WINDOW_W, WINDOW_H, "cub3D");
 }
 
-void	initPlayer(t_player *P)
+void	init_player(t_player *P)
 {
 	P->x = WINDOW_W / 2;
 	P->y = WINDOW_H / 2;
@@ -53,12 +54,10 @@ void	initPlayer(t_player *P)
 
 void	init(t_data *dt)
 {
-
 	WINDOW_W = dt->Tmap.row * tileSize;
 	WINDOW_H = dt->Tmap.col * tileSize;
 	NUM_RAYS = WINDOW_W;
-
-	initMlx(&dt->Tmlx);
-	initPlayer(&dt->P);
-	initImage(dt, &dt->Timg, &dt->Tmlx);
+	init_mlx(&dt->Tmlx);
+	init_player(&dt->P);
+	init_image(dt, &dt->Timg, &dt->Tmlx);
 }
